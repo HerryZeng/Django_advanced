58c5bfd0fadc828a3f61cc36ba2df708a3520933
Herry <herry.zeng@gmail.com>
2018-01-17 01:55:04 +0800

    第三次修改

---
+++ firstapp/forms.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from django import forms
+
+from .models import Task, Poem
+
+
+class AddTaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['task_id', 'name']
+
+
+class AddPoemForm(forms.ModelForm):
+    class Meta:
+        model = Poem
+        fields = ['author', 'title']
